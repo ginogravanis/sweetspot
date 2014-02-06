0c9465f342ddd29e3bdaa95cec7081c8811a9d69
Gino Gravanis <gino.gravanis@gmail.com>
2014-02-06 14:00:46 +0100

    Rename greenpixel and redpixel to green and red respectively.

--- SweetSpot 2.0/SweetSpot 2.0/Screens/SampleScreen.cs
+++ SweetSpot 2.0/SweetSpot 2.0/Screens/SampleScreen.cs
@@ -8,22 +8,18 @@ namespace SweetSpot_2._0
     public class SampleScreen : Screen
     {
         ContentManager content;
-        RenderTarget2D screen;
         Texture2D image;
-        Texture2D redPixel;
-        Texture2D greenPixel;
+        Texture2D red;
+        Texture2D green;
         Effect effect;
         float effectAmount = 1f;
 
         public override void LoadContent()
         {
             content = new ContentManager(ScreenManager.Game.Services, "Content");
-            screen = new RenderTarget2D(ScreenManager.GraphicsDevice,
-                ScreenManager.GraphicsDevice.PresentationParameters.BackBufferWidth,
-                ScreenManager.GraphicsDevice.PresentationParameters.BackBufferHeight);
             image = content.Load<Texture2D>("testimage");
-            redPixel = content.Load<Texture2D>("redpixel");
-            greenPixel = content.Load<Texture2D>("greenpixel");
+            red = content.Load<Texture2D>("red");
+            green = content.Load<Texture2D>("green");
             effect = content.Load<Effect>("SaturationShader");
         }
 
@@ -55,27 +51,21 @@ namespace SweetSpot_2._0
             SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
             Viewport viewport = graphics.Viewport;
 
-            graphics.SetRenderTarget(screen);
-            spriteBatch.Begin();
-            spriteBatch.Draw(image, new Rectangle(0, 0, viewport.Width, viewport.Height), Color.White);
-            spriteBatch.End();
-            graphics.SetRenderTarget(null);
-
             effect.Parameters["effectAmount"].SetValue(effectAmount);
             graphics.Clear(Color.Black);
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Opaque, null, null, null, effect);
-            spriteBatch.Draw((Texture2D)screen, new Rectangle(0, 0, viewport.Width, viewport.Height), Color.White);
+            spriteBatch.Draw(image, new Rectangle(0, 0, viewport.Width, viewport.Height), Color.White);
             spriteBatch.End();
 
             Vector2 sweetSpotPosition = WorldToScreenCoords(viewport.Bounds, ScreenManager.Kinect.sweetSpot);
             Rectangle sweetSpot = new Rectangle((int)sweetSpotPosition.X - 10, (int)sweetSpotPosition.Y - 10, 20, 20);
             spriteBatch.Begin();
-            spriteBatch.Draw(greenPixel, sweetSpot, Color.White);
+            spriteBatch.Draw(green, sweetSpot, Color.White);
             if (ScreenManager.Kinect.IsViewerActive())
             {
                 Vector2 viewerPosition = WorldToScreenCoords(viewport.Bounds, ScreenManager.Kinect.GetViewerPosition());
                 Rectangle viewer = new Rectangle((int)viewerPosition.X - 15, (int)viewerPosition.Y - 15, 30, 30);
-                spriteBatch.Draw(redPixel, viewer, Color.White);
+                spriteBatch.Draw(red, viewer, Color.White);
             }
             spriteBatch.End();
         }
