d16ed19f1bdc651d6f83918cafd299859ec53c98
Gino Gravanis <gino.gravanis@gmail.com>
2014-02-19 14:31:36 +0100

    Refactor EffectScreen.

---
+++ SweetSpot 2.0/SweetSpot 2.0/Screens/ImageScreen.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SweetSpot_2._0
+{
+    public class ImageScreen : Screen
+    {
+        protected Texture2D image;
+
+        public ImageScreen(ScreenManager screenManager, Texture2D image)
+            : base(screenManager)
+        {
+            this.image = image;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+            Viewport viewport = ScreenManager.GraphicsDevice.Viewport;
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(image, new Rectangle(0, 0, viewport.Width, viewport.Height), Color.White);
+            spriteBatch.End();
+        }
+    }
+}
