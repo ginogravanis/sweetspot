7e7a719cbfb25042f41923d42a020404b18852e8
Gino Gravanis <gino.gravanis@gmail.com>
2014-03-02 20:46:23 +0100

    Add calibration screen.

--- SweetSpot 2.0/SweetSpot 2.0/Screens/EffectDebugScreen.cs
+++ SweetSpot 2.0/SweetSpot 2.0/Screens/EffectDebugScreen.cs
@@ -17,8 +17,8 @@ namespace SweetSpot_2._0
         public override void LoadContent()
         {
             base.LoadContent();
-            green = content.Load<Texture2D>("texture\\green");
-            red = content.Load<Texture2D>("texture\\red");
+            green = Content.Load<Texture2D>("texture\\green");
+            red = Content.Load<Texture2D>("texture\\red");
         }
 
         public override void Draw(GameTime gameTime)
@@ -29,21 +29,14 @@ namespace SweetSpot_2._0
             Viewport viewport = ScreenManager.GraphicsDevice.Viewport;
 
             // Overlay
-            Vector2 sweetSpotPosition = WorldToScreenCoords(viewport.Bounds, ScreenManager.Kinect.sweetSpot);
-            Vector2 viewerPosition = WorldToScreenCoords(viewport.Bounds, ScreenManager.Kinect.GetViewerPosition());
+            Vector2 sweetSpotPosition = SensorManager.WorldToScreenCoords(viewport.Bounds, ScreenManager.Kinect.sweetSpot);
+            Vector2 viewerPosition = SensorManager.WorldToScreenCoords(viewport.Bounds, ScreenManager.Kinect.GetViewerPosition());
             Rectangle sweetSpot = new Rectangle((int)sweetSpotPosition.X - 10, (int)sweetSpotPosition.Y - 10, 20, 20);
             Rectangle viewer = new Rectangle((int)viewerPosition.X - 15, (int)viewerPosition.Y - 15, 30, 30);
             spriteBatch.Begin();
             spriteBatch.Draw(green, sweetSpot, Color.White);
             spriteBatch.Draw(red, viewer, Color.White);
             spriteBatch.End();
-        }
-
-        protected Vector2 WorldToScreenCoords(Rectangle viewport, Vector2 position)
-        {
-            float x = (viewport.Width / 2) + (int)(position.X * 250);
-            float y = (int)(position.Y * 250);
-            return new Vector2(x, y);
         }
     }
 }
---
+++ SweetSpot 2.0/SweetSpot 2.0/Screens/SensorPanel.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SweetSpot_2._0
+{
+    class SensorPanel
+    {
+        protected Screen container;
+        protected Texture2D green;
+        protected Texture2D red;
+        protected Rectangle bounds;
+        protected Rectangle sensorRect;
+        protected const int rectangleSize = 20;
+
+        protected Sensor sensor;
+        protected bool SensorActive = false;
+        protected Vector2 SensorPosition;
+        protected List<Vector2> userPositions;
+
+        public SensorPanel(Screen container, Sensor sensor, Rectangle screenBounds)
+        {
+            this.container = container;
+            this.sensor = sensor;
+            bounds = screenBounds;
+            SensorPosition = new Vector2(screenBounds.Center.X, 0);
+            userPositions = new List<Vector2>();
+            sensorRect = new Rectangle(
+                screenBounds.Center.X - rectangleSize/2,
+                0,
+                rectangleSize,
+                rectangleSize
+                );
+        }
+
+        protected Rectangle makeUserRect(Vector2 position)
+        {
+            Vector2 screenPosition = SensorManager.WorldToScreenCoords(bounds, position);
+
+            return new Rectangle(
+                (int)screenPosition.X - rectangleSize / 2,
+                (int)screenPosition.Y - rectangleSize / 2,
+                rectangleSize,
+                rectangleSize
+                );
+        }
+
+        public void LoadContent()
+        {
+            green = container.Content.Load<Texture2D>("texture\\green");
+            red = container.Content.Load<Texture2D>("texture\\red");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (sensor.HasActiveUsers())
+            {
+                SensorActive = true;
+                userPositions = sensor.GetUserPositions();
+            }
+            else
+            {
+                SensorActive = false;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(red, sensorRect, Color.White);
+            if (SensorActive)
+            {
+                foreach (Vector2 position in userPositions)
+                {
+                    spriteBatch.Draw(green, makeUserRect(position), Color.White);
+                }
+            }
+        }
+    }
+}
