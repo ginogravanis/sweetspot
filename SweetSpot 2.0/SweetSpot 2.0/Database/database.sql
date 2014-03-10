6566738403558561dc88a033f79536cb5c4f70db
Gino Gravanis <ginogravanis@users.noreply.github.com>
2014-03-10 11:01:12 +0100

    Update database schema.

--- SweetSpot 2.0/SweetSpot 2.0/Database/database.sql
+++ SweetSpot 2.0/SweetSpot 2.0/Database/database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "calibration" ("device" VARCHAR NOT NULL , "axis_tilt" FLOAT NOT NULL , "translate_x" FLOAT NOT NULL , "translate_y" FLOAT NOT NULL );
+CREATE TABLE "calibration" ("deviceId" VARCHAR NOT NULL , "axis_tilt" FLOAT NOT NULL , "translate_x" FLOAT NOT NULL , "translate_y" FLOAT NOT NULL , "translate_z" FLOAT NOT NULL , "created" DATETIME NOT NULL  DEFAULT CURRENT_TIMESTAMP);
 CREATE TABLE "sweetspot" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "x" FLOAT, "y" FLOAT);
 CREATE TABLE "test" ("id" INTEGER PRIMARY KEY  NOT NULL ,"subject" INTEGER NOT NULL ,"screen" VARCHAR NOT NULL ,"begin" DATETIME NOT NULL  DEFAULT (CURRENT_TIMESTAMP) ,"sweetspot" INT DEFAULT (null) );
 CREATE TABLE "user_position" ("test" INTEGER,"timestamp" INTEGER,"x" FLOAT,"y" FLOAT);
