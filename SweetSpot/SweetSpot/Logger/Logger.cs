bd5e3d902abccf4a309ff94bbce3a172dcda9548
Gino Gravanis <ginogravanis@users.noreply.github.com>
2014-03-12 19:01:20 +0100

    Add logging.

---
+++ SweetSpot/SweetSpot/Logger/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using SweetSpot.Util;
+
+namespace SweetSpot
+{
+    public static class Logger
+    {
+        const string fileName = "sweetspot.log";
+        public static void Log(string msg)
+        {
+            FileStream stream = new FileStream(fileName, FileMode.OpenOrCreate);
+            StreamWriter file = new StreamWriter(stream);
+            file.WriteLine(String.Format("[{0}] {1}", DateTime.Now.GetTimestamp(), msg));
+            file.Close();
+            stream.Close();
+        }
+    }
+}
