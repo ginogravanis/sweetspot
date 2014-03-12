e85393e714fbcb5c795c27b47c3df1dbfc2870a6
Gino Gravanis <ginogravanis@users.noreply.github.com>
2014-03-12 16:22:36 +0100

    Generate test session after calibration.

--- SweetSpot 2.0/SweetSpot 2.0/ScreenManagement/ScreenManager.cs
+++ SweetSpot 2.0/SweetSpot 2.0/ScreenManagement/ScreenManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SweetSpot_2._0.Database;
@@ -12,15 +13,13 @@ namespace SweetSpot_2._0.ScreenManagement
         public bool Debug { get; internal set; }
 
         protected List<Screen> screens;
-
         protected List<Vector2> sweetSpots;
+        protected List<Effect> effects;
+        protected Texture2D image;
 
         public SensorManager Kinect { get; internal set; }
-
         public InputManager Input { get; internal set; }
-
         public IDatabase Database { get; internal set; }
-
         public SpriteBatch SpriteBatch { get; internal set; }
 
         public ScreenManager(Game game)
@@ -29,6 +28,7 @@ namespace SweetSpot_2._0.ScreenManagement
             Debug = false;
             screens = new List<Screen>();
             sweetSpots = new List<Vector2>();
+            effects = new List<Effect>();
             Kinect = new SensorManager();
             Input = new InputManager();
             Database = new SQLiteAdapter();
@@ -44,33 +44,17 @@ namespace SweetSpot_2._0.ScreenManagement
         {
             SpriteBatch = new SpriteBatch(GraphicsDevice);
 
-            Texture2D image = Game.Content.Load<Texture2D>("texture\\testimage");
-            Effect saturation = Game.Content.Load<Effect>("shader\\SaturationShader");
-            Effect brightness = Game.Content.Load<Effect>("shader\\BrightnessShader");
-            Effect contrast = Game.Content.Load<Effect>("shader\\ContrastShader");
-            Effect pixelate = Game.Content.Load<Effect>("shader\\PixelateShader");
-            Effect distort = Game.Content.Load<Effect>("shader\\DistortShader");
-            Effect jitter = Game.Content.Load<Effect>("shader\\JitterShader");
+            image = Game.Content.Load<Texture2D>("texture\\testimage");
+
+            effects.Add(Game.Content.Load<Effect>("shader\\SaturationShader"));
+            effects.Add(Game.Content.Load<Effect>("shader\\BrightnessShader"));
+            effects.Add(Game.Content.Load<Effect>("shader\\ContrastShader"));
+            effects.Add(Game.Content.Load<Effect>("shader\\PixelateShader"));
+            effects.Add(Game.Content.Load<Effect>("shader\\DistortShader"));
+            effects.Add(Game.Content.Load<Effect>("shader\\JitterShader"));
 
             AddScreen(new TransitionScreen(this, "Calibration"));
             AddScreen(new SensorCalibrationScreen(this));
-            AddScreen(new TransitionScreen(this, "Arrow"));
-            AddScreen(new BaselineArrowScreen(this, image));
-            AddScreen(new TransitionScreen(this, "Text"));
-            AddScreen(new BaselineTextScreen(this, image));
-            AddScreen(new TransitionScreen(this, "Saturation"));
-            AddScreen(new EffectScreen(this, image, saturation));
-            AddScreen(new TransitionScreen(this, "Brightness"));
-            AddScreen(new EffectScreen(this, image, brightness));
-            AddScreen(new TransitionScreen(this, "Contrast"));
-            AddScreen(new EffectScreen(this, image, contrast));
-            AddScreen(new TransitionScreen(this, "Pixelate"));
-            AddScreen(new EffectScreen(this, image, pixelate));
-            AddScreen(new TransitionScreen(this, "Distort"));
-            AddScreen(new EffectScreen(this, image, distort));
-            AddScreen(new TransitionScreen(this, "Jitter"));
-            AddScreen(new EffectScreen(this, image, jitter));
-            AddScreen(new TransitionScreen(this, "Thank you for participating!"));
         }
 
         protected override void UnloadContent()
@@ -79,15 +63,30 @@ namespace SweetSpot_2._0.ScreenManagement
                 screen.UnloadContent();
         }
 
-        public void AddScreen(Screen newScreen)
+        public void AddScreen(Screen screen)
         {
-            newScreen.LoadContent();
-            screens.Add(newScreen);
+            screen.LoadContent();
+            screens.Add(screen);
         }
 
         public void GenerateTestSession(List<Vector2> sweetSpots)
         {
             this.sweetSpots = sweetSpots;
+            List<Screen> testSession = new List<Screen>();
+            testSession.Add(new BaselineArrowScreen(this, image));
+            testSession.Add(new BaselineTextScreen(this, image));
+            foreach (Effect effect in effects)
+            {
+                testSession.Add(new EffectScreen(this, image, effect));
+            }
+            testSession.Shuffle();
+
+            for (int i = 0; i < testSession.Count; i++)
+            {
+                AddScreen(new TransitionScreen(this, String.Format("Test {0}/{1}", i + 1, testSession.Count)));
+                AddScreen(testSession[i]);
+            }
+            AddScreen(new TransitionScreen(this, "Thank you for participating!"));
         }
 
         public void ToggleDebug()
@@ -97,21 +96,20 @@ namespace SweetSpot_2._0.ScreenManagement
 
         public override void Update(GameTime gameTime)
         {
-            Input.Update(gameTime);
-            Kinect.Update(gameTime);
-            screens[0].Update(gameTime);
-
             if (screens[0].Finished)
                 RemoveScreen();
 
-            if (screens.Count == 0)
-                Game.Exit();
+            Input.Update(gameTime);
+            Kinect.Update(gameTime);
+            screens[0].Update(gameTime);
         }
 
         protected void RemoveScreen()
         {
             screens[0].UnloadContent();
             screens.RemoveAt(0);
+            if (screens.Count == 0)
+                Game.Exit();
         }
 
         public override void Draw(GameTime gameTime)
